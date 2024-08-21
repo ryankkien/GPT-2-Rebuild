f31dff71bdf955f213458f3fc57d6ec966a41754
ryankkien <93295033+ryankkien@users.noreply.github.com>
2024-08-20 17:21:10 -0700

    gpt weights copying

--- train_gpt2.py
+++ train_gpt2.py
@@ -66,10 +66,12 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig: #params
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int =12
+    n_head: int =12
+    n_embd: int = 768
+
 class GPT(nn.Module):
     def __init__(self, config):
         super().__init__() 
@@ -82,8 +84,39 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd) #final layer
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) #768 x 50257
+    
+    @classmethod
+    def from_pretrained(cls, model_type):
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print(f"Loading pretrained model {model_type}")
 
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024),
+            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280),
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
 
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        sd_hf = [k for k in sd_hf if not k.endswith('.attn.masked_bias')]
+        sd_hf = [k for k in sd_hf if not k.endswith('.attn.bias')]
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
 
-
-
+        assert len(sd_keys) == len(sd_hf), f"mismatch: {len(sd_keys)} != {len(sd_hf)}"
+        for k in sd_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
